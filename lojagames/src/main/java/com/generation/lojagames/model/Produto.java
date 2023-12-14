6ce548c5698a590a84d2214b6e0658f251e66387
Natan Oliveira <natanoliveirasantosmed@gmail.com>
2023-12-14 15:12:45 -0300

    “LojaGamesPart2”

---
+++ lojagames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,81 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produtos") // CREATE TABLE tb_produtos
+public class Produto {
+
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Long id;
+
+	@NotBlank(message = "O Nome do jogo é obrigatório!")
+	private String nome;
+
+	private String descricao;
+
+	private String dataLancamento;
+
+	@NotNull(message = "O preço do produto é obrigatório!")
+	private BigDecimal preco;
+
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(String dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
